b73d1603332919d4f52e1e4ac1c2d28c4a3f5d72
Vapurrmaid <vapurrmaid@gmail.com>
2019-07-07 00:42:51 -0400

    Initial commit

---
+++ seed-data.js
@@ -0,0 +1,14 @@
+db.Books.insertMany([
+    {
+        "Name": "Design Patterns",
+        "Price": 54.93,
+        "Category": "Computers",
+        "Author": "Ralph Johnson"
+    },
+    {
+        "Name": "Clean Code",
+        "Price": 43.15,
+        "Category": "Computers",
+        "Author": "Robert C. Martin"
+    }
+]);
